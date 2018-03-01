972a4ce4de474206ce93abba2bfa7ca7a7a16b8f
bindumadhvai <bindumadhavi1996@gmail.com>
2018-03-01 10:20:24 +0530

    changes made

--- dply.sh
+++ dply.sh
@@ -1,20 +1,17 @@
-#cloning the application
-rm -rf Java_sample_app
-git clone https://github.com/bindumadhvai/Java_sample_app
-cd Java_sample_app/SpringMVCSecurityXML/
+cd SpringMVCSecurityXML/
 
 #Building the application
 mvn clean install >> f1.txt
 
 #Renaming the war file
-mv /var/lib/jenkins/workspace/ddd/Java_sample_app/SpringMVCSecurityXML/target/SpringMVCSecurityXML.war /var/lib/jenkins/workspace/ddd/Java_sample_app/SpringMVCSecurityXML/target/bindu.war
+mv /var/lib/jenkins/workspace/BuildDeploy3/SpringMVCSecurityXML/target/SpringMVCSecurityXML.war /var/lib/jenkins/workspace/BuildDeploy3/SpringMVCSecurityXML/target/bindu.war
 
 #Running the test.sh script to check and intsall tomcat in remote server
-ssh ggk@54.244.209.186 'bash -s' -- <  /home/ubuntu/rmdp/test.sh
+ssh ggk@54.244.209.186 'bash -s' -- < /var/lib/jenkins/workspace/BuildDeploy3/test.sh
 echo "Tomcat8 is ready to provide services in remote"
 
 #copying the war file into remote
-scp /var/lib/jenkins/workspace/ddd/Java_sample_app/SpringMVCSecurityXML/target/bindu.war ggk@54.244.209.186:/var/lib/tomcat8/webapps
+scp /var/lib/jenkins/workspace/BuildDeploy3/SpringMVCSecurityXML/target/bindu.war ggk@54.244.209.186:/var/lib/tomcat8/webapps
 echo "Deployed successfully"
 
 #checking the application by opening the browser

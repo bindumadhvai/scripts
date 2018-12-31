d6fb2944a7c9bbb40aa85c9731f08b6202b16261
bindumadhvai <31912994+bindumadhvai@users.noreply.github.com>
2018-12-31 14:11:06 +0530

    Update dply.sh

--- dply.sh
+++ dply.sh
@@ -7,13 +7,13 @@ mvn clean install >> f1.txt
 mv /var/lib/jenkins/workspace/BuildDeploy3/bindu/SpringMVCSecurityXML/target/SpringMVCSecurityXML.war /var/lib/jenkins/workspace/BuildDeploy3/bindu/SpringMVCSecurityXML/target/bindu.war
 
 #Running the test.sh script to check and intsall tomcat in remote server
-ssh ggk@54.244.209.186 'bash -s' -- < /var/lib/jenkins/workspace/BuildDeploy3/test.sh
+ssh ggk@ 'bash -s' -- < /var/lib/jenkins/workspace/BuildDeploy3/test.sh
 echo "Tomcat8 is ready to provide services in remote"
 
 #copying the war file into remote
-scp /var/lib/jenkins/workspace/BuildDeploy3/bindu/SpringMVCSecurityXML/target/bindu.war ggk@54.244.209.186:/var/lib/tomcat8/webapps
+scp /var/lib/jenkins/workspace/BuildDeploy3/bindu/SpringMVCSecurityXML/target/bindu.war ggk@:/var/lib/tomcat8/webapps
 echo "Deployed successfully"
 
 #checking the application by opening the browser
-#firefox 54.244.209.186:8080/bindu
+#firefox :8080/bindu
 exit 0
